8ac37df3ed37d3ba068c9c0d7142c366c556fc0c
BobinShihJosh <bos2@uw.edu>
2024-01-26 13:03:59 -0800

    add back url

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import PeelerInfo from './pages/PeelerInfo.jsx';
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename={"/peel_info/"}>
         <Routes>
           <Route index element = { <Home />}></Route>
           <Route path='/Home' element = { <Home />}></Route>
